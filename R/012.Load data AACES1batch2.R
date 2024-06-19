bc1cc24872954d15355724300c1b02770a6b46a7
chrisleitzinger <christelleleitzinger@gmail.com>
2024-06-19 17:02:55 -0400

    Update data loading with new IF data

--- R/012.Load data AACES1batch2.R
+++ R/012.Load data AACES1batch2.R
@@ -1,6 +1,7 @@
 # Import library
 
 library(tidyverse); library(janitor)
+library(haven)
 
 
 ############################################################################## I ### Load new ROIs data----
@@ -20,6 +21,13 @@ ncocs_clinical <-
   read_csv(
     paste0(path,
            "/K99_R00/Image analysis data/AACES MCC18207 mIF Data/ncocs_all.csv"))
+path2 <- fs::path("", "Volumes", "Peres_Research", "AACES2", "Analgesic medications and survival")
+survival_time_Nov2023 <-
+  read_sas(paste0(path2, 
+                  "/data/raw data/analgesics_with_phase2.sas7bdat")) %>% 
+  select(suid, days_int_to_event, vital_status_fin) %>% 
+  rename(vital_status_nov2023 = vital_status_fin) %>% 
+  mutate(suid = as.character(suid))
 #-----------------------------------------------------------------------------------------------------------------
 ROI_tumor_2022jan <-
   readxl::read_xlsx(
@@ -27,21 +35,21 @@ ROI_tumor_2022jan <-
            "/K99_R00/Image analysis data/AACES MCC18207 mIF Data/L.Peres_P1_AACES MCC18207_ROI_Results-2022-1.xlsx"
   ), sheet = "Tumor (PCK+)",
   .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 ROI_stroma_2022jan <-
   readxl::read_xlsx(
     paste0(path,
            "/K99_R00/Image analysis data/AACES MCC18207 mIF Data/L.Peres_P1_AACES MCC18207_ROI_Results-2022-1.xlsx"
   ), sheet = "Stroma (PCK-)",
   .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 ROI_total_2022jan <-
   readxl::read_xlsx(
     paste0(path,
            "/K99_R00/Image analysis data/AACES MCC18207 mIF Data/L.Peres_P1_AACES MCC18207_ROI_Results-2022-1.xlsx"
     ), sheet = "AACES MCC18207 ROI Counts",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names() %>% 
+  janitor::clean_names(replace=janitor:::mu_to_u) %>% 
   rename_at(vars(starts_with("fox") | 
                    starts_with("cd") |
                    starts_with("percent_fox") | 
--- R/013.Load data exhaustion marker_June2024.R
+++ R/013.Load data exhaustion marker_June2024.R
@@ -19,21 +19,21 @@ TMA2017_total_2024june <-
            "/TMA_Data/AACES 2017A (June 2024)/Peres_P3_MOTIF_AACES 2017A TMA_June2024_Results.xlsx"
     ), sheet = "Peres_P3_MOTIF_AACES 2017A TMA_",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 TMA2017_tumor_2024june <-
   readxl::read_xlsx(
     paste0(path,
            "/TMA_Data/AACES 2017A (June 2024)/Peres_P3_MOTIF_AACES 2017A TMA_June2024_Results.xlsx"
     ), sheet = "Tumor",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 TMA2017_stroma_2024june <-
   readxl::read_xlsx(
     paste0(path,
            "/TMA_Data/AACES 2017A (June 2024)/Peres_P3_MOTIF_AACES 2017A TMA_June2024_Results.xlsx"
     ), sheet = "Stroma",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 
 TMA2018_total_2024june <-
   readxl::read_xlsx(
@@ -41,21 +41,21 @@ TMA2018_total_2024june <-
            "/TMA_Data/AACES 2018A (June 2024)/Peres_P3_MOTIF_AACES 2018A TMA_June2024_Results.xlsx"
     ), sheet = "Peres_P3_MOTIF_AACES 2018A TMA_",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 TMA2018_tumor_2024june <-
   readxl::read_xlsx(
     paste0(path,
            "/TMA_Data/AACES 2018A (June 2024)/Peres_P3_MOTIF_AACES 2018A TMA_June2024_Results.xlsx"
     ), sheet = "Tumor",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 TMA2018_stroma_2024june <-
   readxl::read_xlsx(
     paste0(path,
            "/TMA_Data/AACES 2018A (June 2024)/Peres_P3_MOTIF_AACES 2018A TMA_June2024_Results.xlsx"
     ), sheet = "Stroma",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 
 TMAaaces2_total_2024june <-
   readxl::read_xlsx(
@@ -63,21 +63,21 @@ TMAaaces2_total_2024june <-
            "/TMA_Data/TMA 1 (June 2024)/Peres_P3_MOTIF_TMA1_June2024_Results.xlsx"
     ), sheet = "Total_Summary_Results",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 TMAaaces2_tumor_2024june <-
   readxl::read_xlsx(
     paste0(path,
            "/TMA_Data/TMA 1 (June 2024)/Peres_P3_MOTIF_TMA1_June2024_Results.xlsx"
     ), sheet = "Tumor",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 TMAaaces2_stroma_2024june <-
   readxl::read_xlsx(
     paste0(path,
            "/TMA_Data/TMA 1 (June 2024)/Peres_P3_MOTIF_TMA1_June2024_Results.xlsx"
     ), sheet = "Stroma",
     .name_repair = fct_name_repair) %>% 
-  janitor::clean_names()
+  janitor::clean_names(replace=janitor:::mu_to_u)
 #-----------------------------------------------------------------------------------------------------------------
 path1 <- fs::path("","Volumes","Peres_Research", "K99_R00",
                  "Image analysis data", "TMA manifests")
@@ -93,6 +93,12 @@ TMA2018_manifest <-
            "/TMA manifest exhaustion panel/TMAmanifest_2018.xlsx"
     ), sheet = "suid_core_match") %>% 
   janitor::clean_names()
+TMA2024_manifest <-
+  readxl::read_xlsx(
+    paste0(path1,
+           "/TMA manifest exhaustion panel/TMAmanifest_2024.xlsx"
+    ), sheet = "suid_core_match") %>% 
+  janitor::clean_names()
 #-----------------------------------------------------------------------------------------------------------------
 # Use list.files as data was saved over multiple files
 file_list <- list.files(
@@ -102,17 +108,36 @@ file_list <- list.files(
   recursive=FALSE,
   full.names = TRUE)
 
-WTS_total_2024june <- do.call("rbind",lapply(Sys.glob(file_list), readxl::read_xlsx,
-                                          sheet = 1,
-                              .name_repair = fct_name_repair)) %>% 
-  janitor::clean_names()
-WTS_tumor_2024june <- do.call("rbind",lapply(Sys.glob(file_list), readxl::read_xlsx,
-                                             sheet = "Tumor",
-                              .name_repair = fct_name_repair)) %>% 
-  janitor::clean_names()
-WTS_stroma_2024june <- do.call("rbind",lapply(Sys.glob(file_list), readxl::read_xlsx,
-                                             sheet = "Stroma",
-                               .name_repair = fct_name_repair)) %>% 
-  janitor::clean_names()
+# WTS_total_2024june <- do.call("rbind",lapply(Sys.glob(file_list), readxl::read_xlsx,
+#                                           sheet = 1,
+#                               .name_repair = fct_name_repair)) %>% 
+#   janitor::clean_names(replace=janitor:::mu_to_u)
+# WTS_tumor_2024june <- do.call("rbind",lapply(Sys.glob(file_list), readxl::read_xlsx,
+#                                              sheet = "Tumor",
+#                               .name_repair = fct_name_repair)) %>% 
+#   janitor::clean_names(replace=janitor:::mu_to_u)
+# WTS_stroma_2024june <- do.call("rbind", lapply(Sys.glob(file_list), readxl::read_xlsx,
+#                                              sheet = "Stroma",
+#                                .name_repair = fct_name_repair)) %>% 
+#   janitor::clean_names(replace=janitor:::mu_to_u)
+
+df.list <- setNames(lapply(file_list, readxl::read_xlsx,
+                           sheet = 1,
+                           .name_repair = fct_name_repair), str_match(file_list, "SET(.)")[,1])
+WTS_total_2024june <- bind_rows(df.list, .id = "Set") %>% 
+    janitor::clean_names(replace=janitor:::mu_to_u)
+
+df.list <- setNames(lapply(file_list, readxl::read_xlsx,
+                           sheet = "Tumor",
+                           .name_repair = fct_name_repair), str_match(file_list, "SET(.)")[,1])
+WTS_tumor_2024june <- bind_rows(df.list, .id = "Set") %>% 
+    janitor::clean_names(replace=janitor:::mu_to_u)
+
+df.list <- setNames(lapply(file_list, readxl::read_xlsx,
+                           sheet = "Stroma",
+                           .name_repair = fct_name_repair), str_match(file_list, "SET(.)")[,1])
+WTS_stroma_2024june <- bind_rows(df.list, .id = "Set") %>% 
+    janitor::clean_names(replace=janitor:::mu_to_u)
+
 
 # End data load
